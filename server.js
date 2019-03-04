88d07299f25d1285e83e129eb7af097d4937a0be
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2019-03-04 16:38:36 +0530

    added the use case for all the third party libraries

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // set up ======================================================================
 // main server file with all those required configuration
+//Express 3.x is a light-weight web application framework to help organize your web application into an MVC architecture on the server side. You can use a variety of choices for your templating language (like EJS, Jade, and Dust.js).
 var express = require('express');
 var app = express(); 						// create our app w/ express
+// We use a database like MongoDB with Mongoose (for modeling) to provide a backend for your Node.js application. Express.js basically helps you manage everything, from routes, to handling requests and views.
 var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
 var database = require('./config/database'); 			// load the database config
+// morgan is a HTTP request logger middleware for node.js . so that we can can maintain a log of our project.
 var morgan = require('morgan');    // importing morgan library
+//body parser is used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 var bodyParser = require('body-parser');  // using body-parser library to extract information from body
 var methodOverride = require('method-override'); // importing method-overide module
 
