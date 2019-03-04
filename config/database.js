05dd6ee687016d87d446b62a27a6d9e0d7c826a7
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2019-03-04 16:10:40 +0530

    some db related changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // setting up the url for our database located at a remote service provider we can install mongodb locally if we want and give that path here by default mongodb run on port 27017
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : "mongodb://newuser:rahul123@ds023644.mlab.com:23644/developer",
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
